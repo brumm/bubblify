8d0815e58680089f0527527a3dc919290eb762a1
Philipp Brumm <philipp@funkensturm.de>
2011-10-02 22:21:27 +0200

    separated out css js files

---
+++ test.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".bubbles li").bubblify({position: "top"});
+  $(".bubbles li").bubblify({position: "right"});
+  $(".bubbles li").bubblify({position: "left"});
+  $(".bubbles li").bubblify({position: "bottom"});
+});
